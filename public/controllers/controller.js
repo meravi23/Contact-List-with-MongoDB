f0b311de29ccf924b00ba6065ee20c8dc98864a9
Merav <merav.mi@gmail.com>
2019-10-19 12:47:44 +0300

    added refresh function so new contact is shown immediately

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,20 +1,28 @@
-
 var app = angular.module("app", []).controller('AppCtrl',
   function AppCtrl($scope, $http) {
-    
-    // creating route
-    // and telling the browser what to do
-    // when it recieves the json data from the server:
-    $http.get('/contactlist').then(function (response) {
-      console.log("I received the data from the server");
-      $scope.contacts = response.data;
-    });
 
-    $scope.addContact = function(){
+    let refresh = function () {
+      // creating route
+      // and telling the browser what to do
+      // when it recieves the json data from the server:
+      $http.get('/contactlist').then(function (response) {
+        console.log("I received the data from the server");
+        $scope.contacts = response.data;
+        // clearing input fields:
+        $scope.contact = null;
+      });
+    };
+
+    refresh();
+
+    $scope.addContact = function () {
       console.log($scope.contact);
       // sending the input data to the server:
-      $http.post('contactlist', $scope.contact);
-    }
+      $http.post('contactlist', $scope.contact).then(function (res) {
+        console.log(res);
+        refresh();
+      });
+    };
 
   }
 );
