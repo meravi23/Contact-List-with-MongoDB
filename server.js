ce3f211556a9e7231b744e52ffa97df80f368d96
Merav <merav.mi@gmail.com>
2019-10-19 10:24:05 +0300

    switched to retrieving data from mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
-var app = express();
+const app = express();
+const mongojs = require('mongojs');
+const db = mongojs('contactlist', ['contactlist']);
 
 // static is used to serve static files (i.e. not changing dynamically) such as images, CSS files, and JavaScript files
 // function signature is: express. static(root, [options]) 
@@ -9,27 +11,11 @@ app.use(express.static(__dirname + '/public'));
 
 app.get('/contactlist', function (req, res) {
     console.log("I received a GET request");
-    person1 = {
-        name: 'Merav',
-        email: "merav@gmail.com",
-        phone: "054-555-5555"
-    };
-
-    person2 = {
-        name: 'John',
-        email: "john@hotmail.com",
-        phone: "052-222-2222"
-    };
-
-    person3 = {
-        name: 'Jane',
-        email: "jane@yahoo.com",
-        phone: "053-333-3333"
-    };
-
-    var contactList = [person1, person2, person3];
-    // sending contacts in json so that the controller can read it:
-    res.json(contactList);
+    
+    db.contactlist.find(function(err, docs) {
+        console.log(docs);
+        res.json(docs);        
+    })
 });
 
 app.listen(3000);
